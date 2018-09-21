445ea62cc7036b0dc1c48afaa2ab745fb5d24e11
BAECHLER Guillaume <ET02343@zgie.ztech>
2018-09-21 12:03:14 +0200

    Adding transactions & transaction pool logic to p2p app

--- app/index.js
+++ app/index.js
@@ -2,30 +2,49 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
-const p2pServer = new P2pServer(bc);
+const wallet = new Wallet();
+const tp = new TransactionPool();
+const p2pServer = new P2pServer(bc, tp);
 
 app.use(bodyParser.json());
 
 app.get('/blocks', (req, res) => {
-    res.json(bc.chain);
+  res.json(bc.chain);
 });
 
 app.post('/mine', (req, res) => {
-    const block = bc.addBlock(req.body.data);
-    console.log(`New block added: ${block.toString()}`);
+  const block = bc.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
 
-    p2pServer.syncChains();
+  p2pServer.syncChains();
 
-    res.redirect('/blocks');
+  res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+  res.json(tp.transactions);
+});
+
+app.post('/transact', (req, res) => {
+  const { recipient, amount } = req.body;
+  const transaction = wallet.createTransaction(recipient, amount, tp);
+  p2pServer.broadcastTransaction(transaction);
+  res.redirect('/transactions');
+});
+
+app.get('/public-key', (req, res) => {
+  res.json({ publicKey: wallet.publicKey });
 });
 
 app.listen(HTTP_PORT, () => {
-    console.log(`Listening on port ${HTTP_PORT}`);
+  console.log(`Listening on port ${HTTP_PORT}`);
 });
 
 p2pServer.listen();
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -2,10 +2,15 @@ const WebSocket = require('ws');
 
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+const MESSAGE_TYPES = {
+  chain: 'CHAIN',
+  transaction: 'TRANSACTION'
+};
 
 class P2pServer {
-  constructor(blockchain) {
+  constructor(blockchain, transactionPool) {
     this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
     this.sockets = [];
   }
 
@@ -42,10 +47,33 @@ class P2pServer {
 
   messageHandler(socket) {
     socket.on('message', message => {
+      
       const data = JSON.parse(message);
 
-      this.blockchain.replaceChain(data);
+      switch (data.type) {
+        case MESSAGE_TYPES.chain :
+          this.blockchain.replaceChain(data.chain);
+          break;
+        case MESSAGE_TYPES.transaction :
+          this.transactionPool.updateOrAddTransaction(data.transaction);
+          break;
+      }
+
     });
+  }
+
+  sendChain(socket) {
+    socket.send(JSON.stringify({
+      type: MESSAGE_TYPES.chain,
+      chain: this.blockchain.chain
+    }));
+  }
+
+  sendTransaction(socket, transaction) {
+    socket.send(JSON.stringify({
+      type: MESSAGE_TYPES.transaction,
+      transaction
+    }));
   }
 
   syncChains() {
@@ -54,8 +82,10 @@ class P2pServer {
     });
   }
 
-  sendChain(socket) {
-    socket.send(JSON.stringify(this.blockchain.chain));
+  broadcastTransaction(transaction) {
+    this.sockets.forEach( socket => {
+      this.sendTransaction(socket, transaction)
+    });
   }
 }
 

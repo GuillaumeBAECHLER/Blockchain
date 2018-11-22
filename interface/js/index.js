04734e8204cd632dcd8f48aa55666fcbe9edadc0
Guillaume BAECHLER <baechler.guillaume.pro@gmail.com>
2018-11-22 11:44:24 +0100

    last interface changes

---
+++ interface/js/index.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("name");
+  
+    /* Select the text field */
+    copyText.select();
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+  }
